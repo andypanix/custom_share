65201f96238523437c645c57d90b77911d3b37c9
Andrea su Democrito <andrea.panisson@gmail.com>
2014-05-27 11:37:41 +0200

    Rimosso langcode

--- templates/social-share-button.tpl.php
+++ templates/social-share-button.tpl.php
@@ -1,6 +1,6 @@
 <div class="custom_share">
   <!-- AddThis buttons content BEGIN -->
   <a class="button social-share-link" href="#addthis_toolbox_<?php print $entity->nid; ?>">Share</a>
-  <div id="addthis_toolbox_<?php print $entity->nid; ?>" addthis:ui_language="<?php print $langcode; ?>" class="addthis_toolbox addthis_default_style" data-share="share-<?php print $entity->nid; ?>"></div>
+  <div id="addthis_toolbox_<?php print $entity->nid; ?>" class="addthis_toolbox addthis_default_style" data-share="share-<?php print $entity->nid; ?>"></div>
   <!-- AddThis buttons content END -->
 </div>
